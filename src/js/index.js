45912a87bd99d66c06d23d752e36ccfd65ccf342
Dawid Domaradzki <dawid-kun@wp.pl>
2020-08-23 16:57:44 +0200

    feat: js strings/task2 code

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,36 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+// basic strings task #2
+
+const greeting = document.querySelector('.button__stringsTask2--js');
+
+greeting.addEventListener('click', (e) => {
+    var userInput = getInputFromUser();
+
+    if (userInput != null) {
+        var nameAfterProcessing = processUserName(userInput);
+        alert(`Cześć ` + nameAfterProcessing);
+    }
+});
+
+function getInputFromUser() {
+    var result = prompt(`Podaj swoje imię`);
+    if (!result) {
+        alert(`Anulowałeś to ziom`);
+    }
+
+    return result;
+}
+
+function processUserName(userInput) {
+    var splitedName = userInput.toLowerCase().split(' ');
+
+    for (var i = 0; i < splitedName.length; i++) {
+
+        splitedName[i] = splitedName[i].charAt(0).toUpperCase() + splitedName[i].substring(1);
+    }
+
+    return splitedName.join(' ');
+}
