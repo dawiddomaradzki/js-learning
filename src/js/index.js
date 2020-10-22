df4c5e024784f5b9e81d4e9502b43e7e4a6dac7b
Dawid Domaradzki <dawid-kun@wp.pl>
2020-10-22 22:55:24 +0200

    feat: Caesar Cipher input

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,11 @@ import '../scss/main.scss';
     $(this).addClass("active").siblings("li").removeClass("active");
     $("section:nth-of-type(" + $(this).data("rel") + ")")
       .stop()
-      .fadeIn(400, "linear")
+      .fadeIn(200, "linear")
       .siblings("section")
       .stop()
-      .fadeOut(400, "linear");
+      .fadeOut(200, "linear");
+
   });
 })(jQuery);
 
