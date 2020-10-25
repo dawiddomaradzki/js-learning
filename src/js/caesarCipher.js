549ffd187e47884b2cbdd483b4dc73d175bc1bd2
Dawid Domaradzki <dawid-kun@wp.pl>
2020-10-25 12:31:25 +0100

    feat: Caesar Cipher input check/js

--- src/js/caesarCipher.js
+++ src/js/caesarCipher.js
@@ -1,12 +1,25 @@
 // Caesar Cipher
 
 let caesarButton = document.querySelector('.button__caesar--js');
+const alphabet = 'aąbcćdeęfghijklłmnńoóprsśtuwyzźż';
 
-function sConsole(event) {
+const sConsole = (event) => {
     event.preventDefault();
     var textInput = document.getElementById("textInput").value;
+    // let inputArray = textInput.split('');
+
+    if (textInput.length == 0 || textInput.match(/^[0-9]+$/) != null) {
+        throw new Error(`podano puste pole lub ciąg cyfr`);
+    }
+
+    
     return textInput;
 }
+
+// function caesar13(textInput) {
+//     let inputArray = textInput.split('');
+//     return inputArray;
+// }
 
 caesarButton.addEventListener('click', (e) => {
     console.log(sConsole(event));
