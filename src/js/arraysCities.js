782b0eb184ea5b122a973adbe3ec4747a9fc4b0d
Dawid Domaradzki <dawid-kun@wp.pl>
2021-06-01 18:53:55 +0200

    feat: JS arraysCities/task1

--- src/js/arraysCities.js
+++ src/js/arraysCities.js
@@ -1,3 +1,10 @@
 import { cities } from "./cities";
 
 // Arrays Cities task #1
+// Wypisz ile miast jest w Polsce
+
+const citiesTask1 = document.querySelector(".button__arraysCities1--js");
+
+citiesTask1.addEventListener("click", (e) => {
+  console.log(`W tablicy znajduje się ${cities.length} miast`);
+});
