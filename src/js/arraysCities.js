33116ace5b8879f0a03ce03106aaa64bca1d768a
Dawid Domaradzki <dawid-kun@wp.pl>
2021-06-01 19:04:19 +0200

    feat: JS arraysCities/task2

--- src/js/arraysCities.js
+++ src/js/arraysCities.js
@@ -8,3 +8,18 @@ const citiesTask1 = document.querySelector(".button__arraysCities1--js");
 citiesTask1.addEventListener("click", (e) => {
   console.log(`W tablicy znajduje się ${cities.length} miast`);
 });
+
+// Arrays Cities task #2
+// Wypisz ile jest ludzi w tych miastach
+
+const citiesTask2 = document.querySelector(".button__arraysCities2--js");
+
+const allPeople = cities.reduce((total, city) => {
+  return city.people + total;
+}, 0);
+
+citiesTask2.addEventListener("click", (e) => {
+  console.log(
+    `Łącznie w miastach znajdujących się w tablicy jest ${allPeople} ludzi`
+  );
+});
