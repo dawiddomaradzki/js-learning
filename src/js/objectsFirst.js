5b7f00cd9dd421d8c7607f69a3a73af04d254ff0
Dawid Domaradzki <dawid-kun@wp.pl>
2021-07-01 20:44:58 +0200

    feat: JS objectsFirst/task2

--- src/js/objectsFirst.js
+++ src/js/objectsFirst.js
@@ -29,3 +29,30 @@ objectsFirstTask1.addEventListener("click", (e) => {
   rectangle.showArea();
   circle.showArea();
 });
+
+// objectsFirst task #2 //
+
+const objectsFirstTask2 = document.querySelector(".button__objectsFirst2--js");
+
+const currentUser = {
+  name: "Dawid",
+  surname: "Domaradzki",
+  email: "dawid@pl.pl",
+  www: "dawid.pl",
+  userType: "male",
+  isActive: true,
+  show() {
+    return console.log(
+      `Imię: ${this.name}, Nazwisko: ${this.surname}, Email: ${this.email}, www: ${this.www},	Płeć: ${this.userType}, Aktywny: ${this.isActive}`
+    );
+  },
+  setActive(active) {
+    this.isActive = active;
+    return console.log(`Aktywny: ${this.isActive}`);
+  },
+};
+
+objectsFirstTask2.addEventListener("click", (e) => {
+  currentUser.show();
+  currentUser.setActive(false);
+});
