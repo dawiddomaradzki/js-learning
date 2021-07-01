1919a94ebb3480474a3d6e67bc0358e99ee5356c
Dawid Domaradzki <dawid-kun@wp.pl>
2021-07-01 19:22:43 +0200

    feat: JS objectsFirst/task1

--- src/js/objectsFirst.js
+++ src/js/objectsFirst.js
@@ -1,1 +1,31 @@
-// objectsFirst task #1 //
+// objectsFirst task #1 // Stwórz 2 obiekty:rectangle i circle. Obiekt rectangle powinien mieć właściwości height, width, natomiast circle właściwość radius. Oba obiekty powinny mieć metodę showArea(), która zwróci pole danej figury.
+
+const objectsFirstTask1 = document.querySelector(".button__objectsFirst1--js");
+
+const rectangle = {
+  height: 20,
+  width: 5,
+  showArea() {
+    return console.log(
+      `Kwadrat ma szerokość ${this.width} i wysokość ${
+        this.height
+      }. Jego pole wynosi ${this.height * this.width}.`
+    );
+  },
+};
+
+const circle = {
+  radius: 15,
+  showArea() {
+    return console.log(
+      `Koło ma promień ${this.radius}. Jego pole wynosi ${
+        this.radius * this.radius * 3.14
+      }.`
+    );
+  },
+};
+
+objectsFirstTask1.addEventListener("click", (e) => {
+  rectangle.showArea();
+  circle.showArea();
+});
